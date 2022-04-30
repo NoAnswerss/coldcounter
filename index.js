80d99e2e52bfe8682afb4f473bc3a6b18c5c00a5
Vlad Pop <NoAnswerss@users.noreply.github.com>
2022-04-30 21:14:11 +0100

    made counter start from 0 after each save

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let countEl = document.getElementById("count-el")
 count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
     console.log(count)
 }
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
