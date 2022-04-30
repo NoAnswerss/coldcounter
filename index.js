a0080090ce4252d4be7fa30997eeb343e1872a6d
Vlad Pop <NoAnswerss@users.noreply.github.com>
2022-04-30 20:59:24 +0100

    comit increment and save button

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-console.log(countEl)
-
-let count = 0
+count = 0
 
 function increment() {
     count = count + 1
     countEl.innerText = count
     console.log(count)
+}
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
 }
